9428fbd9c3ad0531fde55f2893eebf3bc5ba80e9
vansh-000 <144169922+vansh-000@users.noreply.github.com>
2024-04-15 02:22:22 +0530

    Build converation model

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
 app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
 
 app.get("/",(req, res) => {
     res.send("Hello world !");
