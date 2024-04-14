ca403b0a0e87d2914ea199127de6ec1f6c235705
vansh-000 <144169922+vansh-000@users.noreply.github.com>
2024-04-15 00:59:42 +0530

    Mongo DB connected

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,16 +1,48 @@
-export const login = (req, res, next) =>{
-    res.send("Login");
-    console.log("Login Route");
-}
-export const logout = (req, res, next) =>{
-    console.log("Logout Route");
-}
-export const signup = (req, res, next) =>{
-    try {
-        const { fullName, username, password, confirmPassword, gender } = req.body;
+import User from "../models/user.model.js";
 
-    } catch (error) {
-        console.log("Error in signup controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
+export const signup = async (req, res, next) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
     }
-}
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+    // hashing the passwords
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+        fullName,
+        username,
+        password,
+        gender,
+        profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    
+    res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+    });
+    await newUser.save();
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+export const login = (req, res, next) => {
+  res.send("Login");
+  console.log("Login Route");
+};
+export const logout = (req, res, next) => {
+  console.log("Logout Route");
+};
--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,16 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+app.use(express.json());
+app.use("/api/auth",authRoutes);
 
 app.get("/",(req, res) => {
     res.send("Hello world !");
 })
 
-app.use("/api/auth",authRoutes);
 
 app.listen(PORT,()=>{
     connectToMongoDB();
