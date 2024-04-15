552dab39bf89f0bdd824d6579b2ecaa4e5809621
vansh-000 <144169922+vansh-000@users.noreply.github.com>
2024-04-15 17:23:48 +0530

    stable

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
 	const [loading, setLoading] = useState(false);
-	// const { setAuthUser } = useAuthContext();
+	const { setAuthUser } = useAuthContext();
 
 	const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
 		const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
@@ -21,7 +22,8 @@ const useSignup = () => {
 			if (data.error) {
 				throw new Error(data.error);
 			}
-            console.log(data);
+            localStorage.setItem("chat-user", JSON.stringify(data));
+			setAuthUser(data);
 			
 		} catch (error) {
 			toast.error(error.message);
